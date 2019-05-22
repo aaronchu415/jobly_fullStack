88b133e20ef8cf418241c4772934410387e217de
Parco Tam <ptam1556@gmail.com>
2019-05-22 13:39:53 -0700

    navbar login logic

--- frontend/src/components/common/NavBar.js
+++ frontend/src/components/common/NavBar.js
@@ -1,35 +1,48 @@
-import React, { Component, Link } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
 	state = {};
 	render() {
+		const isLoginNav = (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item mr-4">
+					<Link className="nav-link" href="/companies">
+						Companies
+					</Link>
+				</li>
+				<li className="nav-item mr-4">
+					<Link className="nav-link" href="/jobs">
+						Jobs
+					</Link>
+				</li>
+				<li className="nav-item mr-4">
+					<Link className="nav-link" href="/profile">
+						Profile
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link className="nav-link" href="/">
+						Log out
+					</Link>
+				</li>
+			</ul>
+		);
+		const notLoginNav = (
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item">
+					<Link className="nav-link" href="/login">
+						Log In
+					</Link>
+				</li>
+			</ul>
+		);
 		return (
 			<nav className="Navigation navbar navbar-expand-md">
 				<Link className="navbar-brand" href="/">
 					Jobly
 				</Link>
-				<ul className="navbar-nav ml-auto">
-					<li className="nav-item mr-4">
-						<Link className="nav-link" href="/companies">
-							Companies
-						</Link>
-					</li>
-					<li className="nav-item mr-4">
-						<Link className="nav-link" href="/jobs">
-							Jobs
-						</Link>
-					</li>
-					<li className="nav-item mr-4">
-						<Link className="nav-link" href="/profile">
-							Profile
-						</Link>
-					</li>
-					<li className="nav-item">
-						<Link className="nav-link" href="/">
-							Log out
-						</Link>
-					</li>
-				</ul>
+				{this.props.isLogin ? { isLoginNav } : { notLoginNav }}
 			</nav>
 		);
 	}
