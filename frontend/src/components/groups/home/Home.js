8550779a7b71e4d2ffac00cb61f20f7701482b7d
aaronchu415 <aaronchu415@gmail.com>
2019-05-22 13:28:11 -0700

    home component

--- frontend/src/components/groups/home/Home.js
+++ frontend/src/components/groups/home/Home.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component {
   state = {}
   render() {
-    return (<h1>HOME PAGE</h1>);
+    return (
+      <div class="pt-5">
+        <div class="Home">
+          <div class="container text-center">
+            <h1 class="mb-4 font-weight-bold">Jobly</h1>
+            <p class="lead">All the jobs in one, convenient place.</p><a class="btn btn-primary font-weight-bold"
+              href="/login">Log in</a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
