29269347040dc6be2cc62613018b9397f9e5d6eb
aaronchu415 <aaronchu415@gmail.com>
2019-05-22 13:41:59 -0700

    home component part2

--- frontend/src/components/groups/home/Home.js
+++ frontend/src/components/groups/home/Home.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 class Home extends Component {
-  state = {}
+
+  __renderLoginButton = () => {
+    return <Link class="btn btn-primary font-weight-bold"
+      to="/login">Log in</Link>
+  }
+
+  __renderWelcomeBack = () => {
+    return <h2>Welcome Back!</h2>
+  }
+
   render() {
+
+    const { isLogin } = this.props
+
     return (
       <div class="pt-5">
         <div class="Home">
           <div class="container text-center">
             <h1 class="mb-4 font-weight-bold">Jobly</h1>
-            <p class="lead">All the jobs in one, convenient place.</p><a class="btn btn-primary font-weight-bold"
-              href="/login">Log in</a>
+            <p class="lead">All the jobs in one, convenient place.</p>
+            {isLogin ? this.__renderWelcomeBack() : this.__renderLoginButton()}
           </div>
         </div>
       </div>
     );
   }
+}
+
+Home.defaultProps = {
+  isLogin: false
 }
 
 export default Home;
