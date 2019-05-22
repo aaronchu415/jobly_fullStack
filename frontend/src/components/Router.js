f124ac8cfbb37cde8c94a3bdf32214391eeb587e
aaronchu415 <aaronchu415@gmail.com>
2019-05-22 13:47:55 -0700

    WIP router

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, ReactFragment } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Companies from './groups/company/Companies';
 import Jobs from './groups/job/Jobs';
@@ -6,6 +6,7 @@ import Login from './common/login';
 import Profile from './groups/profile/Profile';
 import Company from './groups/company/Company';
 import Home from './groups/home/Home';
+import NavBar from './common/NavBar';
 
 
 class Router extends Component {
@@ -20,7 +21,7 @@ class Router extends Component {
           <Route exact path="/profile" render={() => <Profile />} />
           <Route exact path="/companies" render={() => <Companies />} />
           <Route exact path="/companies/:handle" render={(routeP) => <Company {...routeP} />} />
-          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/" render={() => <NavBar isLogin={true} />} />
         </Switch>
       </BrowserRouter>);
   }
