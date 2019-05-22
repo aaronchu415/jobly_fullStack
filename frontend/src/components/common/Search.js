bd058865637834a0a7d21182b3721551120c99ec
aaronchu415 <aaronchu415@gmail.com>
2019-05-22 14:53:37 -0700

    companies page

--- frontend/src/components/common/Search.js
+++ frontend/src/components/common/Search.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-	state = {};
+	state = {
+		search: ''
+	};
+
+	handleChange = (evt) => {
+		this.setState({ [evt.target.name]: evt.target.value })
+	}
+
+	handleSubmit = (e) => {
+		const { search } = this.state
+		e.preventDefault();
+		this.props.submit(search);
+		this.setState({ search: '' })
+	}
+
 	render() {
+		const { search } = this.state
+
 		return (
-			<div class="Search mb-4">
-				<form class="form-inline">
+			<div className="Search mb-4">
+				<form className="form-inline" onSubmit={this.handleSubmit}>
 					<input
-						class="form-control form-control-lg flex-grow-1"
+						className="form-control form-control-lg flex-grow-1"
 						name="search"
 						placeholder="Enter search term.."
-						value=""
+						value={search}
+						onChange={this.handleChange}
 					/>
-					<button type="submit" class="btn btn-lg btn-primary">
+					<button type="submit" className="btn btn-lg btn-primary">
 						Submit
 					</button>
 				</form>
--- frontend/src/components/groups/company/CompanyCard.js
+++ frontend/src/components/groups/company/CompanyCard.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 class CompanyCard extends Component {
 	state = {};
 	render() {
-		const { handle, name, description, logo_url } = this.props.data;
+		const { handle, name, description } = this.props;
+
+		const logo_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbmE7-UV09CWq_ba12nUvj-0I1_wwWcOnUa0ALmzdTF03Emt1A'
+
 		return (
-			<Link class="CompanyCard Card card" href={`/companies/${handle}`}>
-				<div class="card-body">
-					<h6 class="card-title d-flex justify-content-between">
-						<span class="text-capitalize">{name}</span>
+			<Link className="CompanyCard Card card" to={`/companies/${handle}`}>
+				<div className="card-body">
+					<h6 className="card-title d-flex justify-content-between">
+						<span className="text-capitalize">{name}</span>
 						<img src={`${logo_url}`} alt={`${name} Logo`} />
 					</h6>
 					<p>{description}</p>
@@ -17,6 +20,13 @@ class CompanyCard extends Component {
 			</Link>
 		);
 	}
+}
+
+CompanyCard.defaultProps = {
+	logo_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbmE7-UV09CWq_ba12nUvj-0I1_wwWcOnUa0ALmzdTF03Emt1A',
+	handle: '',
+	name: '',
+	description: '',
 }
 
 export default CompanyCard;
