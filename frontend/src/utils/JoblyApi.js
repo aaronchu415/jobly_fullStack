09a87605594f44804ba3b51d4359e7e3946928bc
aaronchu415 <aaronchu415@gmail.com>
2019-05-24 14:33:47 -0700

    ready to deploy frontend

--- frontend/src/utils/JoblyApi.js
+++ frontend/src/utils/JoblyApi.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
+console.log(BASE_URL)
+
 class JoblyApi {
   static async request(endpoint, paramsOrData = {}, verb = "get") {
 
